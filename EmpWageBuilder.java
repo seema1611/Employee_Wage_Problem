cf635a04b26d93d49ef0eb6157eb029d58067896
seema1611 <seemarajpure16@gmail.com>
2020-04-16 19:14:34 +0530

    ADDED: Calculate full time wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,14 +1,20 @@
 public class EmpWageBuilder {
 
+      //Constants
+      static final int IS_FULL_TIME=1;
+      static final int EMPLOYEE_RATE_PER_HOUR=20;
+
       public static void main(String args[]) {
-         //Constants
-         int IS_FULL_TIME=1;
+         //Variables
+         int empHrs=0,empWage=0;
 
          double empCheck=Math.floor(Math.random() *10)% 2;
          if( empCheck == IS_FULL_TIME ) {
-               System.out.println("Employee is Present");
+               empHrs=8;
          }else {
-               System.out.println("Employee is Absent");
+               empHrs=0;
          }
-      }
+         empWage = empHrs * EMPLOYEE_RATE_PER_HOUR;
+         System.out.println("Employee Wage: "+empWage);
+     }
 }
