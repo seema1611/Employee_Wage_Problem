e0d3671ceb69b8ece57b84e27458d7e03f578753
seema1611 <seemarajpure16@gmail.com>
2020-04-16 19:28:38 +0530

    ADDED: calculate daily wage using case statement

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -9,13 +9,16 @@ public class EmpWageBuilder {
          //Variables
          int empHrs=0,empWage=0;
 
-         double empCheck=Math.floor(Math.random() *10)% 3;
-         if( empCheck == IS_FULL_TIME ) {
-               empHrs=8;
-         }else if ( empCheck == IS_PART_TIME ) {
-               empHrs=4;
-         }else {
-               empHrs=0;
+         int empCheck=(int) (Math.random() *10)% 3;
+         switch(empCheck) {
+         case IS_FULL_TIME:
+            empHrs=8;
+            break;
+         case IS_PART_TIME:
+            empHrs=4;
+            break;
+         default:
+            break;
          }
          empWage = empHrs * EMPLOYEE_RATE_PER_HOUR;
          System.out.println("Employee Wage: "+empWage);
