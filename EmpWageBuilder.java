cffc0fafb5abeb850dcd7d139b8bc689d387a31e
seema1611 <seemarajpure16@gmail.com>
2020-04-16 19:21:18 +0530

    ADDED: Calculate Full and part time daily wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,16 +1,19 @@
 public class EmpWageBuilder {
 
       //Constants
-      static final int IS_FULL_TIME=1;
+      static final int IS_PART_TIME=1;
+      static final int IS_FULL_TIME=2;
       static final int EMPLOYEE_RATE_PER_HOUR=20;
 
       public static void main(String args[]) {
          //Variables
          int empHrs=0,empWage=0;
 
-         double empCheck=Math.floor(Math.random() *10)% 2;
+         double empCheck=Math.floor(Math.random() *10)% 3;
          if( empCheck == IS_FULL_TIME ) {
                empHrs=8;
+         }else if ( empCheck == IS_PART_TIME ) {
+               empHrs=4;
          }else {
                empHrs=0;
          }
